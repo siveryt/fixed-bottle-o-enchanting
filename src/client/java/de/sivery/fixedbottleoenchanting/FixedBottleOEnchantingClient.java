49eda7086364b528b73e5ecad4f225c43ee80464
Sivery <50551841+siveryt@users.noreply.github.com>
2024-12-23 20:31:00 +0100

    chore: Project setup

---
+++ src/client/java/de/sivery/fixedbottleoenchanting/FixedBottleOEnchantingClient.java
@@ -0,0 +1,10 @@
+package de.sivery.fixedbottleoenchanting;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FixedBottleOEnchantingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
